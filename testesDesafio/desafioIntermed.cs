5a1e5f59a9418c5dbdb88b619e0159d447e48859
galemagalhaes <g.alemagalhaes@gmail.com>
2021-04-14 20:18:53 -0300

    Stack de testes
    Projeto usado no curso "Implementando uma stack de testes de unidade e integrado em um projeto .NET de Crowdfunding"
    Código cedido pelo instrutor

---
+++ testesDesafio/desafioIntermed.cs
@@ -0,0 +1,21 @@
+using System; 
+
+class desaf2 {
+
+    static void Main(string[] args) { 
+        //condições: B > C && D > A &&  C + D > A + B && C > 0 && D > 0 && A for par
+
+        string [] selections = Console.ReadLine().Split(' ');
+        int A = int.Parse(selections[0]);
+        int B = int.Parse(selections[1]);
+        int C = int.Parse(selections[2]);
+        int D = int.Parse(selections[3]);
+
+        if ((B > C )&& (D > A) &&  ((C + D )> (A + B) )&& (C > 0) && (D > 0 )&& (A % 2 == 0 )) { //complete a condicional
+          Console.WriteLine("Valores aceitos");
+        } else {
+          Console.WriteLine("Valores nao aceitos");
+        };
+    }
+
+}
